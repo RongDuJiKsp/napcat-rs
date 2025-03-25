077798267f1918a3c2f2d9f5a927ad8a47716c5e
YuQiJaang <rdjksp@gmail.com>
2025-03-25 15:13:35 +0800

    控制命令的执行

--- plugins/chat_message/src/command_exec/shell.rs
+++ plugins/chat_message/src/command_exec/shell.rs
@@ -1,16 +1,37 @@
 use crate::command_exec::app::{BotCommand, BotCommandBuilder};
-use std::sync::atomic::{AtomicUsize, Ordering};
-use kovi::log::info;
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 
 pub async fn register_shell_cmd() {
     BotCommandBuilder::on_super_command("$shell", |e| exec_shell_cmd(e)).await;
     BotCommandBuilder::on_common_command("$hi", |e| exec_hi(e)).await;
+    BotCommandBuilder::on_common_command("$restart", |e| exec_live(e)).await;
+    BotCommandBuilder::on_common_command("$kill", |e| exec_kill(e)).await;
 }
 static ID: AtomicUsize = AtomicUsize::new(1156);
+static LIVE: AtomicBool = AtomicBool::new(true);
 async fn exec_shell_cmd(e: BotCommand) {
-    info!("Shell Called");
+    if !LIVE.load(Ordering::Relaxed) {
+        return;
+    }
     e.event.reply_and_quote(format!("shell创建成功喵！编号 {}", ID.fetch_add(1, Ordering::Relaxed)))
 }
 async fn exec_hi(e: BotCommand) {
+    if !LIVE.load(Ordering::Relaxed) {
+        return;
+    }
     e.event.reply_and_quote("你好喵！我是一只猫娘喵！前面忘了中间忘了，反正我是一只猫娘喵")
+}
+async fn exec_kill(e: BotCommand) {
+    if !LIVE.load(Ordering::Relaxed) {
+        return;
+    }
+    e.event.reply_and_quote("似了喵");
+    LIVE.store(false, Ordering::Relaxed)
+}
+async fn exec_live(e: BotCommand) {
+    if LIVE.load(Ordering::Relaxed) {
+        return;
+    }
+    e.event.reply_and_quote("复活了喵");
+    LIVE.store(true, Ordering::Relaxed)
 }
