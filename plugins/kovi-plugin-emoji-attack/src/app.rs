6176c29325ea2bd9591c6e4026aae7a71e35e3fd
YuQiJaang <rdjksp@gmail.com>
2025-07-13 19:12:48 +0800

    init

--- plugins/kovi-plugin-emoji-attack/src/app.rs
+++ plugins/kovi-plugin-emoji-attack/src/app.rs
@@ -1,31 +1,19 @@
 use crate::config::EmojiAttackConfig;
 use kovi::PluginBuilder as plugin;
+use kovi::bot::runtimebot::RuntimeBot;
+
 use kovi::tokio::sync::RwLock;
-use kovi_plugin_expand_napcat::NapCatApi;
+
 use std::collections::HashSet;
 use std::sync::{Arc, OnceLock};
-use kovi::bot::runtimebot::RuntimeBot;
+use crate::handle::handle_group_msg;
+
 static EMOJI_ALL_USER: OnceLock<RwLock<HashSet<i64>>> = OnceLock::new();
 pub async fn init() {
-    let bot:Arc<RuntimeBot> = plugin::get_runtime_bot();
+    let bot: Arc<RuntimeBot> = plugin::get_runtime_bot();
     EMOJI_ALL_USER
         .set(RwLock::new(HashSet::new()))
         .expect("init EMOJI_ALL_USER failed");
     EmojiAttackConfig::init(&bot).await.unwrap();
-    plugin::on_group_msg(|e| async move {
-        if !e.is_group() {
-            return;
-        }
-        if !EmojiAttackConfig::get()
-            .allow_monkey_groups
-            .contains(&e.group_id.unwrap())
-        {
-            return;
-        }
-        <RuntimeBot as NapCatApi>::set_msg_emoji_like(
-            &*bot,
-            e.message_id as i64,
-            EmojiAttackConfig::get().emoji.as_str(),
-        );
-    })
+    plugin::on_group_msg(move |e| handle_group_msg(e, bot.clone()))
 }
