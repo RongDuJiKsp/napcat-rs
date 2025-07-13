588da128f7ffd57b9c106927ad34cfdbfb62d878
YuQiJaang <rdjksp@gmail.com>
2025-07-14 00:21:07 +0800

    dep self

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ fn main() {
     build_bot!(
         kovi_plugin_command_exec,
         kovi_plugin_javascript_shell,
+        kovi_plugin_check_alllong,
         chat_message,
+        kovi_plugin_emoji_attack,
     )
     .run();
 }
