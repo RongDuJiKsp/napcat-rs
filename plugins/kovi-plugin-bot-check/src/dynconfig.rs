d5866df767a5d3d4a3c44de55ba72546dd258f2d
YuQiJaang <rdjksp@gmail.com>
2025-08-02 19:45:57 +0800

    添加bandyncfg

---
+++ plugins/kovi-plugin-bot-check/src/dynconfig.rs
@@ -0,0 +1,6 @@
+pub struct BanDynConfig {
+    
+}
+impl BanDynConfig {
+    
+}
---
+++ plugins/kovi-plugin-bot-check/src/lib.rs
@@ -0,0 +1,12 @@
+mod dynconfig;
+
+use kovi::PluginBuilder as plugin;
+
+#[kovi::plugin]
+async fn main() {
+    plugin::on_msg(|event| async move {
+        if event.borrow_text() == Some("hi") {
+            event.reply("hi")
+        }
+    });
+}
