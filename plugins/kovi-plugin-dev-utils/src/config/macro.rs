0d1c146850bc1e13426bc5708e79be9a6ae8915d
YuQiJaang <rdjksp@gmail.com>
2025-08-02 22:25:08 +0800

    add data macro

--- plugins/kovi-plugin-dev-utils/src/config/macro.rs
+++ plugins/kovi-plugin-dev-utils/src/config/macro.rs
@@ -3,7 +3,9 @@ macro_rules! config {
     // 三个参数：显式传入文件名
     ($type:ident, $static_ref:ident, $filename:expr) => {
         impl $type {
-            pub fn init(runtime_bot: &::kovi::RuntimeBot) -> ::core::result::Result<(), ::anyhow::Error> {
+            pub fn init(
+                runtime_bot: &::kovi::RuntimeBot,
+            ) -> ::core::result::Result<(), ::anyhow::Error> {
                 ::kovi_plugin_dev_utils::config::initfn::init_config(
                     runtime_bot,
                     $filename,
@@ -20,5 +22,26 @@ macro_rules! config {
     // 两个参数：使用默认文件名
     ($type:ident, $static_ref:ident) => {
         $crate::config!($type, $static_ref, concat!(stringify!($type), ".json"));
+    };
+}
+#[macro_export]
+macro_rules! data {
+    ($type:ident, $static_name:ident, $file:expr) => {
+        impl $type {
+            pub fn init(runtime_bot: &::kovi::RuntimeBot) -> Result<(), ::anyhow::Error> {
+                ::kovi_plugin_dev_utils::config::initfn::init_data(
+                    runtime_bot,
+                    $file,
+                    &$static_name,
+                )
+            }
+
+            pub fn get() -> ::std::sync::Arc<::kovi::tokio::sync::RwLock<Self>> {
+                $static_name.get().unwrap().clone()
+            }
+        }
+    };
+    ($type:ident, $static_name:ident) => {
+        $crate::impl_data_helper!($type, $static_name, concat!(stringify!($type), ".json"));
     };
 }
