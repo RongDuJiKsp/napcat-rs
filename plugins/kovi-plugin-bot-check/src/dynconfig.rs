981f466e8b6748f61eea3efb7cb12f7c007150b7
YuQiJaang <rdjksp@gmail.com>
2025-08-02 21:29:47 +0800

    add writelist

--- plugins/kovi-plugin-bot-check/src/dynconfig.rs
+++ plugins/kovi-plugin-bot-check/src/dynconfig.rs
@@ -9,12 +9,13 @@ static BAN_CONFIG: OnceLock<BanConfig> = OnceLock::new();
 #[serde_as]
 #[derive(Default, Deserialize, Serialize)]
 pub struct BanConfig {
+    enable_group: Vec<i64>, //启用上下文群组
     #[serde_as(as = "VecSkipError<DisplayFromStr>")]
     chat_regex_list: Vec<Regex>, //触发发言匹配的正则表达式列表
     chat_enable_shut_up: Option<i32>, //触发达到次数自动禁言
-    enable_chat_kick: Option<i32>,    //触发发言ban达到次数时自动ban
+    enable_chat_kick: Option<i32>, //触发发言ban达到次数时自动ban
     enable_invite_ban: Option<InviteBanConfig>, //群内邀请ban处理配置
-    enable_invite_kick: Option<i32>,  //触发邀请ban达到次数时自动ban
+    enable_invite_kick: Option<i32>, //触发邀请ban达到次数时自动ban
 }
 config!(BanConfig, BAN_CONFIG, "ban_config.json");
 #[derive(Default, Deserialize, Serialize)]
