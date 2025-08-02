9df080bca5e1d859038fdc9cb76b10555147fe0c
YuQiJaang <rdjksp@gmail.com>
2025-08-02 21:27:30 +0800

    add struct

--- plugins/kovi-plugin-bot-check/src/lib.rs
+++ plugins/kovi-plugin-bot-check/src/lib.rs
@@ -1,10 +1,10 @@
 mod dynconfig;
 
 use kovi::PluginBuilder as plugin;
-use crate::dynconfig::BanDynConfig;
+use crate::dynconfig::BanConfig;
 
 #[kovi::plugin]
 async fn main() {
     let bot = plugin::get_runtime_bot();
-    BanDynConfig::init(&bot).expect("Failed to initialize BanDynConfig");
+    BanConfig::init(&bot).expect("Failed to initialize BanDynConfig");
 }
